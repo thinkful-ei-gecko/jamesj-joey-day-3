90e90c7662a467d89df0fb7c1b44b14892e985d1
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-08-14 15:05:48 -0400

    arrays in objects drill

---
+++ arrays-in-objects.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const hobbit = {
+  meals: [
+    'breakfast', 
+    'second breakfast', 
+    'elevenses', 
+    'lunch', 
+    'afternoon tea', 
+    'dinner', 
+    'supper'
+  ]
+};
+
+console.log(hobbit.meals[3]);
